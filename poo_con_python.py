8abf455cfd8def248c5acf0b7d4e8c9962ea64f7
alvaronov19 <alvaronovelo19@gmail.com>
2025-01-17 08:47:23 -0600

    Problema Celestial

--- poo_con_python.py
+++ poo_con_python.py
@@ -39,9 +39,60 @@ class Guerrero(Personaje):
         #Llamar a la clase padre
         super().__init__(nombre, fuerza, inteligencia, defensa, vida)
         self.espada = espada
-hercules = Guerrero("Hércules", 80, 50, 100, 100, 5)
+
+    #Perdirle al usurario escoger un arma
+    '''def cambiar_arma(self):
+        opcion = int(input("Elige un arma: (1) Espada de plata, daño 10. (2) Espada de bronce, daño 8"))
+        if opcion == 1:
+            self.espada = 10
+        elif opcion == 2:
+            self.espada = 8
+        else:
+            print("Opcion incorrecta")'''
+
+    #Sobreescribir metodo      
+    def atributos(self):
+        super().atributos()
+        print("-Espada-", self.espada)
+
+    #Sobreescribir el calculo de daño 
+    def dañar(self, enemigo):
+        return self.fuerza * self.espada - enemigo.defensa
+    
+class Mago(Personaje):
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, libro):
+        #Llamar a la clase padre
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.libro = libro
+
+    #Sobreescribir metodo      
+    def atributos(self):
+        super().atributos()
+        print("-Libro-", self.libro)
+
+    #Sobreescribir el calculo de daño 
+    def dañar(self, enemigo):
+        return self.inteligencia * self.libro - enemigo.defensa
+
+trakalosa = Personaje("Trakalosa de Monterrey", 20, 15, 10, 100)
+hercules = Guerrero("Hércules", 20, 15, 10, 100, 5)
+diosito = Mago("Diosito", 20, 15, 10, 100, 5)
+#Imprimir atributos antes del ataque
+trakalosa.atributos()
 hercules.atributos()
-print(hercules.espada)
+diosito.atributos()
+#Ataques
+trakalosa.atacar(hercules)
+hercules.atacar(diosito)
+diosito.atacar(trakalosa)
+#Imprimir atributos antes del ataque
+trakalosa.atributos()
+hercules.atributos()
+diosito.atributos()
+
+#hercules.cambiar_arma()
+#hercules.atributos()
+#print(hercules.espada)
 
 #Variable del constructor vacío de la clase 
 #mi_personaje = Personaje ("Kanye", 90, 100, 80, 100)
